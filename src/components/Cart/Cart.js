e3734520961d9acf578989c861866009c1cc9dcd
mmrahmad <mmrahmad91@gmail.com>
2021-08-05 19:41:00 +0600

    Cart add and remove working

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import {useContext} from 'react'
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem'
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -8,17 +9,25 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItems = (
-    <ul className={classes['cart-items']}>
-      {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
-      ))}
-    </ul>
-  );
-
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id)
+  }
+  const cartItemAddHandler = item => {
+    cartCtx.addItem({...item, amount: 1})
+  }
   return (
     <Modal onHideCart={props.onHideCart}>
-      {cartItems}
+      <ul className={classes['cart-items']}>
+        { cartCtx.items.map(item => (
+          <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)} />
+        )) }
+      </ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
